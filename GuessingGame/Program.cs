b72c89f089496e060366186a6bdbb009aa83854f
itailevi420 <itailevi420@gmail.com>
2021-03-04 17:00:32 +0200

    adding to gussing game

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace GuessingGame
 {
@@ -50,33 +54,63 @@ namespace GuessingGame
             Console.WriteLine("And Wellcom to the Guessing Game!");
             Console.WriteLine(" ");
             Console.WriteLine("Please Enter a Number between 0 and 100 and press ENTER.");
-            int inputNum = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            Regex r = new Regex(@"[a-z]+");
+            while (string.IsNullOrEmpty(input) || r.IsMatch(input))
+            {
+                Console.WriteLine("Please enter a valid number");
+                input = Console.ReadLine();
+            }
+            int inputNum = int.Parse(input);
             Console.WriteLine(space);
             Console.WriteLine("Try to guess the number :)....");
             int inputPlayer = -1;
             int counter = 0;
+            int inputPlayerLast = -1;
+            int diff = 0;
+            int diffLast = 0;
             // can do this way too but don't really understand the consept
             // int? inputPlayer = null;
+
+
             while (!(inputPlayer == inputNum))
             {
+                if (inputPlayer != -1)
+                {
+                    if (inputPlayerLast == -1 || inputPlayerLast == inputPlayer)
+                        Console.WriteLine("!!!טעות");
+                    else if (inputNum < inputPlayer)
+                        diff = inputPlayer - inputNum;
+                    else
+                        diff = inputNum - inputPlayer;
+                    if (inputPlayerLast != -1 && inputPlayer != inputPlayerLast)
+                    {
+                        if (inputNum < inputPlayerLast)
+                            diffLast = inputPlayerLast - inputNum;
+                        else
+                            diffLast = inputNum - inputPlayerLast;
+                        if (diffLast > diff)
+                            Console.WriteLine("!!!מתחמם");
+                        else
+                            Console.WriteLine("!!!מתקרר");
+                    }
+                }
 
+                inputPlayerLast = inputPlayer;
                 counter++;
                 inputPlayer = int.Parse(Console.ReadLine());
-                if (inputPlayer < inputNum)
-                    Console.WriteLine("try a begger number");
-                else //if (inputPlayer > inputNum)
-                    Console.WriteLine("try a smaller number");
 
-
-                if (inputPlayer == inputNum)
-                {
-                    Console.WriteLine("Congratulations your guess is correct ! ! !");
-                    Console.WriteLine("It took you only " + counter + " guesses.");
-                }
             }
+
+            Console.WriteLine("Congratulations your guess is correct ! ! !");
+            Console.WriteLine("It took you only " + counter + " guesses.");
             // Console.WriteLine("Congratulations your guess is correct ! ! !");
             // Console.WriteLine("It took you only " + counter + " guesses.");
 
+            // inputPlayerLast = inputPlayer;
+            // inputPlayer = int.Parse(Console.ReadLine());
+            // Console.WriteLine("inputPlayer: " + inputPlayer);
+            // Console.WriteLine("inputPlayerLast: " + inputPlayerLast);
         }
     }
 }
