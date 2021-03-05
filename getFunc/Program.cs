16d5b1c2da2dc6448333c53b132bce8261e543c4
itailevi420 <itailevi420@gmail.com>
2021-03-05 13:17:56 +0200

    blaaaaaaa

---
+++ getFunc/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace getFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string input;
+            input = Console.ReadLine();
+            Console.WriteLine(input);
+            int i, j, l, numPern, pernFlag, symFlag;
+            i = 0;
+            j = 0;
+            l = 0;
+            symFlag = -1;
+            pernFlag = -1;
+            numPern = -1;
+            char c;
+            char cj;
+            string symb = "+-*/%";
+            string pern = "()";
+            string strNumPern = "";
+            string subStrNumPern = "";
+            char minus, plus, multi, dev, mod, pern1, pern2;
+            string my = "";
+            string evalPren = "";
+            string numStr = "";
+            while (i < input.Length)
+            {
+                pernFlag = -1;
+                c = input[i];
+                if (c == pern[0] || c == pern[1])
+                {
+                    strNumPern = strNumPern + i;
+                    pernFlag = 0;
+                }
+                if (pernFlag == 0)
+                {
+                    while (l < strNumPern.Length)
+                    {
+                        if ( == input[i + strNumPern.Length])
+                        {
+                            pernFlag = 1;
+                        }
+                        if (pernFlag != -1 && pernFlag != 1)
+                        {
+
+                            subStrNumPern = subStrNumPern + (input[i + strNumPern.Length]);
+                            numPern = int.Parse(subStrNumPern);
+                        }
+                    }
+                    j = j + 1;
+
+                }
+                i = i + 1;
+            }
+            Console.WriteLine("String: " + strNumPern);
+            Console.WriteLine("int: " + numPern);
+
+
+
+            // int num = int.Parse(input);
+            // Console.WriteLine(num);
+        }
+    }
+}
---
+++ starPir/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace starPir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int num = 0;
+            Console.WriteLine("Please enter a number and press ENTER...");
+            string input = Console.ReadLine();
+            num = int.Parse(input);
+            int i, j, itr, firstAst, lastAst;
+            j = num + (num - 1);
+            i = 0;
+            itr = 1;
+            firstAst = num - 2;
+            lastAst = num;
+            while (itr < num)
+            {
+                while (i < j)
+                {
+                    if (i == num - 1)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+
+                    i = i + 1;
+                }
+                i = 0;
+                Console.WriteLine(" ");
+                while (i < j)
+                {
+                    if (i == firstAst || i == lastAst)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+
+                    i = i + 1;
+                }
+                lastAst = lastAst + 1;
+                firstAst = firstAst - 1;
+                itr = itr + 1;
+            }
+            // while (i < j)
+            // {
+            //     if (i == num - 1)
+            //     {
+            //         Console.Write("*");
+            //     }
+            //     else
+            //     {
+
+            //         Console.Write("+");
+            //     }
+            //     i = i + 1;
+
+            // }
+
+            // while (i < num - 1)
+            // {
+            //     Console.Write("+");
+            //     i = i + 1;
+            // }
+            // Console.Write("*");
+            // i = 0;
+            // while (i < num - 1)
+            // {
+
+            //     Console.Write("+");
+            //     i = i + 1;
+
+            // }
+
+
+        }
+    }
+}
